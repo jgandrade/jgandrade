7609c2cbe23e1449c7456030a2ab97edd1555fd1
jgandrade <96435394+jgandrade@users.noreply.github.com>
2022-01-19 08:34:18 +0800

    Delete title-case-a-sentence.js

--- title-case-a-sentence.js
+++
@@ -1,8 +0,0 @@
-function titleCase(str) {
-  str=str.toLowerCase().split(" ").map(e =>  e[0].toUpperCase().concat(e.slice(1,e.length))).join(" ");
-
-  console.log(str);
-  return str;
-}
-
-titleCase("I'm a little tea pot");
